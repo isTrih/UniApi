4b977cb656fc674e6dc7c05a8afbd616ec98f600
HUA Haohui <hhh@bbc.edu.cn>
2024-01-31 12:06:48 +0800

    fix the sms service

--- internal/controller/sms/sms_v1_sms.go
+++ internal/controller/sms/sms_v1_sms.go
@@ -19,12 +19,26 @@ func (c *ControllerV1) Sms(ctx context.Context, req *v1.SmsReq) (res *v1.SmsRes,
 	}
 	//存储验证码
 	md := g.Model("sms")
-	result, err := md.Insert(smsdata)
-	if err != nil {
-		//错误
-	} else {
-		fmt.Println(result)
-		g.Log("成功")
+	//验证是否存在验证码
+	noEmplty, err := md.Where("phone", req.Phone).One()
+	if err == nil {
+		if noEmplty != nil {
+			//手机号已经存在，更新数据
+			result, err := md.Where("phone", req.Phone).Update(smsdata)
+			if err != nil {
+				//错误
+			} else {
+				fmt.Printf("更新成功%v\n", result)
+			}
+		} else {
+			//验证码为空，插入数据
+			result, err := md.Insert(smsdata)
+			if err != nil {
+				//错误
+			} else {
+				fmt.Printf("插入成功%v\n", result)
+			}
+		}
 	}
 	//发送短信
 	sms := service.SmsHandler{
